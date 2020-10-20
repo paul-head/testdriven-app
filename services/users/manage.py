79a5bfbc474d02d38f1dc864712cc61fa49e1ced
Pavel Golovanov <publichead@yandex.ru>
2020-10-20 18:43:49 +0300

    add base tests for config and users
    * add new cli command

--- services/users/manage.py
+++ services/users/manage.py
@@ -1,5 +1,6 @@
 from flask.cli import FlaskGroup
 from project import app, db
+import unittest
 
 
 cli = FlaskGroup(app)
@@ -7,9 +8,20 @@ cli = FlaskGroup(app)
 
 @cli.command()
 def recreate_db():
+    """ Recreate db """
     db.drop_all()
     db.create_all()
     db.session.commit()
+
+
+@cli.command()
+def test():
+    """ Runs the tests without code coverage """
+    tests = unittest.TestLoader().discover('project/tests', pattern='test*.py')
+    results = unittest.TextTestRunner(verbosity=2).run(tests)
+    if results.wasSuccessful():
+        return 0
+    return 1
 
 
 if __name__ == '__main__':
