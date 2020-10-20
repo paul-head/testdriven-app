c39ba1ee809be044e7993af028ffe9bed3d09642
Pavel Golovanov <publichead@yandex.ru>
2020-10-20 14:01:46 +0300

    add postgres container
    * edit config
    * add model in __init__.py , flask-sqlalchemy
    * add users-db sevice
    * add entrypoint.sh to check db

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,6 +1,7 @@
 import os
+import datetime
 from flask import Flask, jsonify
-
+from flask_sqlalchemy import SQLAlchemy
 
 # instantiate app
 app = Flask(__name__)
@@ -9,6 +10,23 @@ app = Flask(__name__)
 # set config
 app_settings = os.getenv('APP_SETTINGS')
 app.config.from_object(app_settings)
+
+
+# instantiate db
+db = SQLAlchemy(app)
+
+
+# User model
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=True)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
 
 
 @app.route('/users/ping', methods=['GET'])
