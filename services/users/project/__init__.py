0360b21dec20c9d73d4a69f0b321dfdfacbee287
Pavel Golovanov <publichead@yandex.ru>
2020-10-26 16:59:23 +0300

    add flask debugtoolbar, edit project/config and tests_config

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,9 +1,11 @@
 import os
 from flask import Flask
+from flask_debugtoolbar import DebugToolbarExtension
 from flask_sqlalchemy import SQLAlchemy
 
 
 db = SQLAlchemy()
+toolbar = DebugToolbarExtension()
 
 
 def create_app(script_info=None):
@@ -15,6 +17,7 @@ def create_app(script_info=None):
 
     # set up extensions
     db.init_app(app)
+    toolbar.init_app(app)
 
     # register blueprint
     from project.api.users import users_blueprint
