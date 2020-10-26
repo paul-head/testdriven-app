987a759fbddf57570c99d00cf68a6ebad080e1d8
Pavel Golovanov <publichead@yandex.ru>
2020-10-26 14:06:30 +0300

    add coverage and flask8
    * refactor styling errors

--- services/users/manage.py
+++ services/users/manage.py
@@ -1,11 +1,23 @@
+import unittest
+import coverage
 from flask.cli import FlaskGroup
 from project import create_app, db
-import unittest
 from project.api.models import User
 
 
 app = create_app()
 cli = FlaskGroup(create_app=create_app)
+
+
+COV = coverage.coverage(
+    branch=True,
+    include='project/*',
+    omit=[
+        'project/tests/*',
+        'project/config.py',
+    ]
+)
+COV.start()
 
 
 @cli.command()
@@ -32,6 +44,22 @@ def seed_db():
     db.session.add(User(username='pavel', email='pavel@pavel.ru'))
     db.session.add(User(username='testuser1', email='testuser1@testuser1.com'))
     db.session.commit()
+
+
+@cli.command()
+def cov():
+    """ Run unit tests with coverage """
+    tests = unittest.TestLoader().discover('project/tests')
+    results = unittest.TextTestRunner(verbosity=2).run(tests)
+    if results.wasSuccessful():
+        COV.stop()
+        COV.save()
+        print("Coverage Summary:")
+        COV.report()
+        COV.html_report()
+        COV.erase()
+        return 0
+    return 1
 
 
 if __name__ == '__main__':
