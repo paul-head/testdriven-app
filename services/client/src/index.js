5d55bcbb532ac1835bc11d00e6572b95f7f381a3
Pavel Golovanov <publichead@yandex.ru>
2020-11-02 20:06:15 +0300

    send ajax and update the page

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -29,8 +29,16 @@ class App extends Component {
 
     addUser(event) {
         event.preventDefault();
-        console.log('sanity check!');
-        console.log(this.state);
+        const data = {
+            username: this.state.username,
+            email: this.state.email,
+        };
+        axios.post('http://localhost:5001/users', data)
+            .then((res) => {
+                this.getUsers();
+                this.setState({ username: '', email: '' });
+            })
+            .catch((err) => { console.log(err); });
     };
 
     handleChange(event) {
